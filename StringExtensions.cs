4b69d6e8319862894c73d83744bc2d4057e33747
s.zorin <s.zorin@logexpert.ru>
2018-08-07 12:11:34 +0300

    add string extend

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SharpExtension
@@ -26,6 +27,15 @@ namespace SharpExtension
         public static string GetNullIfNullOrWhiteSpace(this string source)
         {
             return source.IsNullOrWhiteSpace() ? null : source;
+        }
+
+        public static bool ContainsAny(this string target, params string[] options)
+            => options.Any(opt => target.Contains(opt));
+
+        public static bool ContainsAny(this string target, bool ignoreCase = false, params string[] options)
+        {
+            if(!ignoreCase) return target.ContainsAny(options);
+            else return target.ToLower().ContainsAny( options.Select(x => x.ToLower()).ToArray() );
         }
     }
 }
