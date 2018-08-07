1b0d61188a04cbdad53f00ed2f3c1e22fcdbcae7
s.zorin <s.zorin@logexpert.ru>
2018-08-07 12:11:17 +0300

    add date extend

--- DateTimeExtension.cs
+++ DateTimeExtension.cs
@@ -5,9 +5,30 @@ namespace SharpExtension
 {
     public static class DateTimeExtension
     {
-        public static TimeSpan FromNow(this DateTime time)
+        public static readonly DateTime EpochZeroTime = new DateTime(1970, 1, 1);
+
+        public static TimeSpan ToNow(this DateTime time)
         {
             return DateTime.Now - time;
         }
+
+        public static bool Greater(this DateTime time, DateTime greatCheck)
+        {
+            return time > greatCheck;
+        }
+
+        public static bool Less(this DateTime time, DateTime greatCheck)
+        {
+            return time < greatCheck;
+        }
+
+        public static DateTime GetDay(this DateTime t)
+            => new DateTime(t.Year, t.Month, t.Day);
+
+        public static long ToEpoch(this DateTime t)
+            => (long) (t - EpochZeroTime).TotalSeconds;
+
+        // public static DateTime ToDateTime(this double seconds) => EpochZeroTime.AddSeconds(seconds);
+        public static DateTime ToDateTime(this long seconds) => EpochZeroTime.AddSeconds(seconds);
     }
 }
